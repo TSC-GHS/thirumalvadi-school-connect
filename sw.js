2b13f277e706b2109ab5fe134c29e9e15690e391
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-08 01:09:05 +0530

    Add service worker with caching functionality
    Implement service worker for caching resources.

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
+const CACHE_NAME = "school-connect-tn-v1";
 
+const urlsToCache = [
+  "/thirumalvadi-school-connect/",
+  "/thirumalvadi-school-connect/index.html",
+  "/thirumalvadi-school-connect/style.css",
+  "/thirumalvadi-school-connect/firebase.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
