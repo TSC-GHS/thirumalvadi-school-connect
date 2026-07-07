2df980be07e80a5f289cdd335ad38c3960758d47
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-07 21:38:56 +0530

    Implement parent EMIS management functions
    Added functions to manage parent EMIS in localStorage.

--- parent_session.js
+++ parent_session.js
@@ -1,1 +1,46 @@
+// Parent Session Manager
 
+export function saveParentEMIS(emis){
+
+localStorage.setItem("parentEMIS", emis);
+
+}
+
+export function getParentEMIS(){
+
+return localStorage.getItem("parentEMIS");
+
+}
+
+export function clearParentEMIS(){
+
+localStorage.removeItem("parentEMIS");
+
+}
+// Check Parent Login
+
+export function isParentLoggedIn(){
+
+return getParentEMIS() !== null;
+
+}
+
+// Auto Redirect
+
+export function requireParentLogin(){
+
+const emis = getParentEMIS();
+
+if(!emis){
+
+alert("Please Login First");
+
+window.location.href = "login.html";
+
+return null;
+
+}
+
+return emis;
+
+}
