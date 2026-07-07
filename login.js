963524472e7ac11877e9949298fa6914a90baa6b
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-07 15:10:07 +0530

    Add loginUser function for user authentication
    Implement user login functionality with role-based redirection.

--- login.js
+++ login.js
@@ -1,1 +1,34 @@
+import { auth } from "./firebase.js";
+import {
+signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
 
+window.loginUser = async function () {
+
+const email = document.getElementById("email").value.trim();
+const password = document.getElementById("password").value.trim();
+const role = document.getElementById("role").value;
+
+try {
+
+await signInWithEmailAndPassword(auth, email, password);
+
+if(role === "Headmaster"){
+location.href = "headmaster.html";
+}
+else if(role === "Teacher"){
+location.href = "teacher_dashboard.html";
+}
+else if(role === "Parent"){
+location.href = "parent_dashboard.html";
+}
+else if(role === "Student"){
+location.href = "student.html";
+}
+
+}
+catch(error){
+alert("Login Failed\n\n" + error.message);
+}
+
+}
