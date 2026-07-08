b34d326a740a7bb081b40d2d0c2566f685209c4c
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-08 23:13:47 +0530

    Implement saveNotice function for Firestore
    Added functionality to save notices to Firestore with validation for input fields.

--- js/notice_management.js
+++ js/notice_management.js
@@ -1,1 +1,39 @@
+import { db } from "../firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+window.saveNotice = async function () {
+
+  const title = document.getElementById("noticeTitle").value.trim();
+  const message = document.getElementById("noticeMessage").value.trim();
+  const priority = document.getElementById("priority").value;
+  const publishDate = document.getElementById("publishDate").value;
+
+  if (!title || !message || !priority || !publishDate) {
+    alert("Please fill all fields");
+    return;
+  }
+    await addDoc(collection(db, "notices"), {
+
+    title: title,
+    message: message,
+    priority: priority,
+    publishDate: publishDate,
+
+    status: "Active",
+
+    createdAt: new Date().toISOString()
+
+  });
+
+  alert("✅ Notice Saved Successfully");
+
+  document.getElementById("noticeTitle").value = "";
+  document.getElementById("noticeMessage").value = "";
+  document.getElementById("priority").value = "";
+  document.getElementById("publishDate").value = "";
+
+}
