fc153d2f6f873c76e3bfb4a0f9b9c5525f370766
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-08 17:56:16 +0530

    Implement homework saving feature
    Added functionality to save homework data to Firestore with validation for input fields.

--- js/homework_management.js
+++ js/homework_management.js
@@ -1,1 +1,39 @@
+import { db } from "../firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+
+window.saveHomework = async function () {
+
+  const className = document.getElementById("classFilter").value;
+  const section = document.getElementById("sectionFilter").value;
+  const dueDate = document.getElementById("dueDate").value;
+  const homework = document.getElementById("homeworkText").value.trim();
+
+  if (!className || !section || !dueDate || !homework) {
+    alert("Please fill all fields");
+    return;
+  }
+    await addDoc(collection(db, "homework"), {
+
+    class: className,
+    section: section,
+    dueDate: dueDate,
+    homework: homework,
+
+    status: "Active",
+
+    createdAt: new Date().toISOString()
+
+  });
+
+  alert("✅ Homework Saved Successfully");
+
+  document.getElementById("classFilter").value = "";
+  document.getElementById("sectionFilter").value = "";
+  document.getElementById("dueDate").value = "";
+  document.getElementById("homeworkText").value = "";
+
+}
