ce9dc8c5f07053c8fa17897d87b5b7eddb49202b
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-09 07:36:05 +0530

    Add session management with authentication checks

--- js/session.js
+++ js/session.js
@@ -1,1 +1,40 @@
+import { auth } from "../firebase.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+
+window.checkSession = function () {
+
+  onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+      alert("Please Login First");
+
+      window.location.href = "roles.html";
+
+    }
+
+  });
+
+};
+window.logout = async function () {
+
+  try {
+
+    await auth.signOut();
+
+    alert("Logged Out Successfully");
+
+    window.location.href = "roles.html";
+
+  }
+
+  catch (error) {
+
+    alert(error.message);
+
+  }
+
+};
