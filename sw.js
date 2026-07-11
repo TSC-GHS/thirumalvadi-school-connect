19b4d3d819f3ccd7c7a1183dc4329a434bba7eca
TSC-GHS <thirumalvadi.schoolconnect@gmail.com>
2026-07-11 18:56:05 +0530

    Update service worker for cache versioning and fetch

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "school-connect-tn-v1";
+const CACHE_NAME = "school-connect-tn-v2";
 
 const urlsToCache = [
   "/thirumalvadi-school-connect/",
@@ -7,18 +7,58 @@ const urlsToCache = [
   "/thirumalvadi-school-connect/firebase.js"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install",(event)=>{
+
+self.skipWaiting();
+
+event.waitUntil(
+
+caches.open(CACHE_NAME).then((cache)=>{
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate",(event)=>{
+
+event.waitUntil(
+
+caches.keys().then((keys)=>{
+
+return Promise.all(
+
+keys.map((key)=>{
+
+if(key!==CACHE_NAME){
+
+return caches.delete(key);
+
+}
+
+})
+
+);
+
+})
+
+);
+
+self.clients.claim();
+
+});
+
+self.addEventListener("fetch",(event)=>{
+
+event.respondWith(
+
+fetch(event.request)
+
+.catch(()=>caches.match(event.request))
+
+);
+
 });
